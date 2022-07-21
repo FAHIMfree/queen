3c7f799e2b695320de5edcc2656b0a7f653006d1
FAHIMfree <109755642+FAHIMfree@users.noreply.github.com>
2022-07-22 03:06:09 +0600

    Add files via upload

---
+++ Fahim.py
@@ -0,0 +1,19 @@
+
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '32bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support Tolls Enjoy\033[1;37m")
+if __name__ == "__Fahim__":
+	try:
+		__import__("idz")._login()
+	except Exception as e:
+		exit(str(e))
+elif bit == '64bit':
+    os.system('xdg-open https://www.facebook.com/profile.php?id=100000778950790')
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
